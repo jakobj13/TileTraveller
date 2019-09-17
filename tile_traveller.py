acb5177082e1a057b6b3656478ef17d2d271c6ab
unknown <jakobj13@ru.is>
2019-09-17 12:01:28 +0000

    Finished program, ugly block of text in while loop -_-

--- tile_traveller.py
+++ tile_traveller.py
@@ -9,6 +9,7 @@ west = False
 east = False
 position = [1,1]
 choice = "n"
+bad_choice_counter = 0
 
 def move(choice,position):
     if choice == 'n' or choice == 'N':
@@ -24,8 +25,18 @@ def move(choice,position):
     return position
 
 def check_valid_direction(choice):
-    if choice == "n" or choice == "N" or choice == "s" or choice == "S" or choice == "w" or choice == "W" or choice == "e" or choice == "E" :
-        return True 
+    if choice == "n" or choice == "N":
+        if north:
+            return True
+    elif choice == "s" or choice == "S":
+        if south:
+            return True
+    elif choice == "w" or choice == "W":
+        if west:
+            return True
+    elif choice == "e" or choice == "E":
+        if east:
+            return True
     else:
         return False     
 
@@ -33,43 +44,69 @@ def get_choice():
     choice = input('Direction: ')
     return choice
 
-def print_directions(position):
-
-    return None
-
-if position == [1,1]:
-    north = True
-if position == [1,2]:
-    south = True
-    east = True
-    north = True
-if position == [1,3]:
-    south = True
-    east = True
-if position == [2,1]:
-    north = True
-if position == [2,2]:
-    south = True
-    west = True
-if position == [2,3]:
-    east = True
-    west = True
-if position == [3,1]:
-    north = True
-if position == [3,2]:
-    north = True
-    south = True
-if position == [3,3]:
-    west = True
-    south = True
-
 while position != [3,1]:
+    if position == [1,1] and bad_choice_counter == 0:
+        north = True
+        south = False
+        east = False
+        west = False
+        print("You can travel: (N)orth.")
+    if position == [1,2] and bad_choice_counter == 0:
+        south = True
+        east = True
+        north = True
+        west = False
+        print("You can travel: (N)orth or (E)ast or (S)outh.")
+    if position == [1,3] and bad_choice_counter == 0:
+        north = False
+        west = False
+        south = True
+        east = True
+        print("You can travel: (E)ast or (S)outh.")
+    if position == [2,1] and bad_choice_counter == 0:
+        north = True
+        west = False
+        east = False
+        south = False
+        print("You can travel: (N)orth.")
+    if position == [2,2] and bad_choice_counter == 0:
+        north = False
+        east = False
+        south = True
+        west = True
+        print("You can travel: (S)outh or (W)est.")
+    if position == [2,3] and bad_choice_counter == 0:
+        north = False
+        south = False
+        east = True
+        west = True
+        print("You can travel: (E)ast or (W)est.")
+    if position == [3,1] and bad_choice_counter == 0:
+        north = True
+        south = False
+        west = False
+        east = False
+    if position == [3,2] and bad_choice_counter == 0:
+        north = True
+        east = False
+        west = False
+        south = True
+        print("You can travel: (N)orth or (S)outh.")
+    if position == [3,3] and bad_choice_counter == 0:
+        north = False
+        east = False
+        west = True
+        south = True
+        print("You can travel: (S)outh or (W)est.")
     choice = get_choice()
     if choice == 'q':
         break
     elif check_valid_direction(choice):
+        bad_choice_counter = 0
         position = move(choice, position)
     else: 
         print('Not a valid direction!')
-    #print(position)
+        bad_choice_counter += 1
+if position == [3,1]:
+    print("Victory!")
 
