6379cf072edd010f3fc1334af5c54e74f1fd120c
unknown <jakobj13@ru.is>
2019-09-17 11:18:56 +0000

    move added

--- tile_traveller.py
+++ tile_traveller.py
@@ -3,15 +3,25 @@ First check position. Identify open walls in the position and print out the opti
 When player moves, check position again and print options. If invalid direction is entered allow the
 player to choose again. End game when victory location is reached
 """
-position = (1,1)
 north = False
 south = False
 west = False
 east = False
+position = [1,1]
+choice = "n"
 
 def move(choice,position):
-
-    return None
+    if choice == 'n' or choice == 'N':
+        position[1] = position[1] + 1
+    elif choice == 's' or choice == 'S':
+        position[1] = position[1] - 1
+    elif choice == 'w' or choice =='W':
+        position[0] = position[0] - 1
+    elif choice == 'e' or choice == 'E':
+        position[0] = position[0] + 1
+    else:
+        return position
+    return position
 
 def get_choice():
     choice = input('Direction: ')
@@ -19,35 +29,36 @@ def get_choice():
 
 def print_directions(position):
 
-    return
-choice = 'h'
-if position == (1,1):
+    return None
+
+if position == [1,1]:
     north = True
-if position == (1,2):
+if position == [1,2]:
     south = True
     east = True
     north = True
-if position == (1,3):
+if position == [1,3]:
     south = True
     east = True
-if position == (2,1):
+if position == [2,1]:
     north = True
-if position == (2,2):
+if position == [2,2]:
     south = True
     west = True
-if position == (2,3):
+if position == [2,3]:
     east = True
     west = True
-if position == (3,1):
+if position == [3,1]:
     north = True
-if position == (3,2):
+if position == [3,2]:
     north = True
     south = True
-if position == (3,3):
+if position == [3,3]:
     west = True
     south = True
 
-while position != (3,1) and choice != "q":
+while position != [3,1] and choice != "q":
     choice = get_choice()
-    move(choice, position)
+    position = move(choice, position)
+    #print(position)
 
