7d2a52b8653b9c83af515dd01e22fef33a37c6e9
unknown <jakobj13@ru.is>
2019-09-17 10:07:25 +0000

    Algorithm commented

---
+++ tile_traveller.py
@@ -0,0 +1,5 @@
+"""
+First check position. Identify open walls in the position and print out the options for the player.
+When player moves, check position again and print options. If invalid direction is entered allow the
+player to choose again. End game when victory location is reached
+"""
